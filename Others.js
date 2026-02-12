4139c8b245f6c5ea8bb1a863ceb1b2d885b6810a
abdullahalnoman003 <noman22622@gmail.com>
2026-02-12 17:29:37 +0600

    Array Operations

---
+++ Others.js
@@ -0,0 +1,17 @@
+//  Arrays and other operations 
+
+const array = [10, 20, 30, 40];
+
+console.log(array);
+
+//  we can modify array elements by using push, pop, shift unshift.
+
+array.push(50); // adds element in the last 
+console.log(array);
+array.pop(); // deletes element from the last
+console.log(array);
+array.shift(); // removes element from the beginning
+console.log(array);
+array.unshift(200); // adds element in the beginning 
+console.log(array);
+

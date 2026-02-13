c636338e20d87e274defc53dd7023a73ecf70e7a
abdullahalnoman003 <noman22622@gmail.com>
2026-02-13 23:25:05 +0600

    Some extra array operations

--- Others.js
+++ Others.js
@@ -1,4 +1,4 @@
-//  Arrays and other operations 
+//  Arrays and other operations
 
 const array = [10, 20, 30, 40];
 
@@ -6,12 +6,53 @@ console.log(array);
 
 //  we can modify array elements by using push, pop, shift unshift.
 
-array.push(50); // adds element in the last 
+array.push(50); // adds element in the last
 console.log(array);
 array.pop(); // deletes element from the last
 console.log(array);
 array.shift(); // removes element from the beginning
 console.log(array);
-array.unshift(200); // adds element in the beginning 
+array.unshift(200); // adds element in the beginning
 console.log(array);
 
+//  Searching and checking an element that are available or not in array
+
+// includes() used to check if that value is available or not
+if (array.includes(20)) {
+  console.log("Array contains the element");
+} else {
+  console.log("Element is not available");
+}
+
+if (array.includes(120)) {
+  console.log("Array contains the element");
+} else {
+  console.log("Element is not available");
+}
+
+// index value of an element
+const index = array.indexOf(30);
+console.log(index); // if the element is present will return the position and if not available it will return -1
+
+const MulArray = [10, 20, 20, 30, 40, 30, 50, 25, 35, 55, 78];
+const present = MulArray.find((num) => num === 30); // find receives an function to find
+console.log(present);
+
+//  ForEach loop
+
+MulArray.forEach((item) => console.log(item));
+
+// Map(), a most used thing
+
+const double = MulArray.map((n) => n * 2); // map accepts a function
+console.log(double);
+
+// filter function
+
+const even = MulArray.filter((n) => n % 2 === 0);
+console.log(even);
+
+//  Spread operators ,
+
+const newArray = [0,0, ...MulArray, 1, 2, 3, 4]; // this operator means to copy things from previous arrays 
+console.log(newArray);
